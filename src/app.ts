131d4a8bc3b3a46e3f9fe47cc3547034c4e0f6d4
Jahid Hasan <info.jhshakil@gmail.com>
2025-10-22 23:55:01 +0600

    update cors url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://brainbin-theta.vercel.app",
+    ],
     credentials: true,
   })
 );
